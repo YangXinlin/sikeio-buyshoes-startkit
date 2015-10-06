4d1ad38ce8d30deb2fe2512169c9b8ea74d67fc3
yangxinlin <yangxinlins@gmail.com>
2015-10-06 23:35:03 +0800

    第八课后半部分

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
-window.onload = function() {
-  makeCartScrollNicely();
-}
-
 function makeCartScrollNicely() {
   var cart = document.querySelector('.cart');
   Ps.initialize(cart);
 }
+
+window.onload = function() {
+  makeCartScrollNicely();
+}
+
+window.onload = function() {
+  var $toggle = document.querySelector(".site__right-sidebar-toggle");
+  $toggle.addEventListener("click",function() {
+    document.body.classList.toggle("js-show-right-sidebar");
+  });
+
+  console.log("page loaded");
+  makeCartScrollNicely();
+};
